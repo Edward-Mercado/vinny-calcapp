bbbe5759dd94157e60aecdbed3582d845d122599
Edward-Mercado <edwardmercado149@gmail.com>
2026-03-04 20:15:36 -0500

    ajsdfadsf

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+hello world
+    </div>
+    <InputBox></InputBox>
+</template>
+
+<script setup>
+import InputBox from '@/components/InputBox.vue'
+
+</script>
+
+<style scoped>
+
+</style>
